ae4d6e8fbcffcc5b81544727e02a65e83e5396b9
unknown <iurygol@gmail.com>
2022-04-29 16:37:37 -0300

    feat: start text robot

---
+++ robots/text.js
@@ -0,0 +1,55 @@
+const sentenceBoundaryDetection = require('sbd')
+
+
+async function robot(content) {
+   
+    //fetchContentFromWikipedia(content)
+   sanitizeContent(content)
+   breakContentIntoSentences(content)
+   console.log('Build Sentences')
+
+   //function fetchContentFromWikipedia(content) { }
+
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+        content.sourceContentSanitized = withoutDatesInParentheses
+
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n')
+
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')) {
+                    return false
+                }
+
+                return true
+            })
+
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+    }
+
+
+    function removeDatesInParentheses(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+    }
+
+    function breakContentIntoSentences(content) {
+        content.sentences = []
+
+        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+        sentences.forEach((sentence) => {
+            content.sentences.push({
+                text: sentence,
+                keywords: [],
+                images: []
+            })
+        })
+    }
+}
+
+
+
+module.exports = robot
